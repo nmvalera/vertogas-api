55fe253bc85cbf3fadf0231d36e5ec69757c2e2b
Nicolas Maurice <ricemo1989@msn.com>
2017-06-16 16:50:38 +0200

    update tests script organization

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,31 +1,35 @@
 import pytest
 from sqlalchemy.exc import IntegrityError
 
-from app.common.constants import NEW_CERTIFICATE_EVENT_NAME, TRANSFER_CERTIFICATE_EVENT_NAME, \
-    CLAIM_CERTIFICATE_EVENT_NAME, ADMIN_CLEANING_EVENT_NAME
-from app.tokens.constants import CONTRACT_TABLENAME, EVENT_TABLENAME, LOG_TABLENAME, TOKEN_TABLENAME
 from app.tokens.helpers import TokenHelpers
 from app.tokens.models import Contract, Event
 from .testconf import CONTRACT_ADDRESS, CONTRACT_FAKE_ADDRESS, CONTRACT_ABI, \
-    EVENT_NAMES, EVENT_ABIS
+    EVENT_NAMES, EVENT_ABIS, make_new_certificate_log, make_transfer_certificate_log, make_claim_certificate_log,\
+    make_admin_clean_certificate_log
 
 
-def test_init_db():
+def setup_module(module):
+    """Setup database for testing"""
     token_helpers = TokenHelpers()
-    tables = token_helpers.init_db()
-    assert len(tables) == 4
-    for table_name in [CONTRACT_TABLENAME, EVENT_TABLENAME, LOG_TABLENAME, TOKEN_TABLENAME]:
-        assert table_name in tables
+    token_helpers.init_db()
+    token_helpers.commit()
+
+
+def teardown_module(module):
+    """Tear down database"""
+    token_helpers = TokenHelpers()
+    token_helpers.drop_db()
+    token_helpers.commit()
+    token_helpers.close()
 
 
 def _test_insert_new_contract(contract_address, contract_abi):
     token_helpers = TokenHelpers()
     contracts_count = len(token_helpers.get_contracts())
     contract = token_helpers.insert_contract(contract_address, contract_abi)
-    assert len(TokenHelpers().get_contracts()) == contracts_count
     token_helpers.commit()
-    assert len(TokenHelpers().get_contracts()) == contracts_count + 1
-    contract = TokenHelpers().get_contract(contract.id)
+    assert len(token_helpers.get_contracts()) == contracts_count + 1
+    contract = token_helpers.get_contract(contract.id)
     assert isinstance(contract, Contract)
     assert contract.abi == contract_abi
     assert contract.is_listening
@@ -41,7 +45,9 @@ def _test_insert_existing_contract(contract_address, contract_abi):
     contracts_count = len(token_helpers.get_contracts())
     with pytest.raises(IntegrityError):
         token_helpers.insert_contract(contract_address, contract_abi)
-    assert len(TokenHelpers().get_contracts()) == contracts_count
+    token_helpers.rollback()
+    token_helpers.commit()
+    assert len(token_helpers.get_contracts()) == contracts_count
 
 
 def test_insert_contract():
@@ -72,21 +78,24 @@ def test_get_event():
         _test_get_event(i)
 
 
-def _test_stop_start_listening(contract_id):
+def _test_stop_listening(contract_id):
     token_helpers = TokenHelpers()
     token_helpers.stop_listening(contract_id)
-    assert TokenHelpers().get_contract(contract_id).is_listening
     token_helpers.commit()
-    assert not TokenHelpers().get_contract(contract_id).is_listening
+    assert not token_helpers.get_contract(contract_id).is_listening
+
+
+def _test_start_listening(contract_id):
+    token_helpers = TokenHelpers()
     token_helpers.start_listening(contract_id)
-    assert not TokenHelpers().get_contract(contract_id).is_listening
     token_helpers.commit()
-    assert TokenHelpers().get_contract(contract_id).is_listening
+    assert token_helpers.get_contract(contract_id).is_listening
 
 
 def test_stop_start_listening():
     for contract_id in range(1, 3):
-        _test_stop_start_listening(contract_id)
+        _test_stop_listening(contract_id)
+        _test_start_listening(contract_id)
 
 
 def _test_set_last_block(contract_id, new_last_block):
@@ -94,12 +103,11 @@ def _test_set_last_block(contract_id, new_last_block):
     contract = token_helpers.get_contract(contract_id)
     former_last_block = contract.last_block
     token_helpers.set_last_block(contract_id, new_last_block)
-    assert TokenHelpers().get_contract(contract_id).last_block == former_last_block
     token_helpers.commit()
     assert token_helpers.get_contract(contract_id).last_block == new_last_block
     token_helpers.set_last_block(contract_id, former_last_block)
     token_helpers.commit()
-    assert TokenHelpers().get_contract(contract_id).last_block == former_last_block
+    assert token_helpers.get_contract(contract_id).last_block == former_last_block
 
 
 def test_set_last_block():
@@ -110,10 +118,9 @@ def _test_create_new_token(contract_id, certificate_id, meta_data, owner):
     token_helpers = TokenHelpers()
     tokens_count = len(token_helpers.get_tokens(contract_id))
     token = token_helpers.create_token(contract_id, certificate_id, meta_data, owner)
-    assert len(TokenHelpers().get_tokens(contract_id)) == tokens_count
     token_helpers.commit()
-    assert len(TokenHelpers().get_tokens(contract_id)) == tokens_count + 1
-    token = TokenHelpers().get_token(id=token.id)
+    assert len(token_helpers.get_tokens(contract_id)) == tokens_count + 1
+    token = token_helpers.get_token(id=token.id)
     assert token.contract_id == contract_id
     assert token.meta_data == meta_data.encode()
     assert token.owner == owner
@@ -126,7 +133,9 @@ def _test_create_existing_token(contract_id, certificate_id, meta_data, owner):
     tokens_count = len(token_helpers.get_tokens(contract_id))
     with pytest.raises(IntegrityError):
         token_helpers.create_token(contract_id, certificate_id, meta_data, owner)
-    assert len(TokenHelpers().get_tokens(contract_id)) == tokens_count
+    token_helpers.rollback()
+    token_helpers.commit()
+    assert len(token_helpers.get_tokens(contract_id)) == tokens_count
 
 
 def test_create_token():
@@ -139,27 +148,28 @@ def test_create_token():
             _test_create_new_token(contract_id, certificate_id, meta_data, owner)
     _test_create_existing_token(1, 'beef', 'meta_data_bis', '0x123434314')
 
+
 def _test_token_content_equals(t1, t2):
     assert t1.certificate_id == t2.certificate_id
     assert t1.meta_data == t2.meta_data
 
+
 def _test_authorized_transfer_token(contract_id, certificate_id, from_address, to_address):
     token_helpers = TokenHelpers()
-    token = TokenHelpers().get_token(contract_id=contract_id, certificate_id=certificate_id)
+    token = token_helpers.get_token(contract_id=contract_id, certificate_id=certificate_id)
     token_helpers.transfer_token(contract_id, certificate_id, from_address, to_address)
-    assert token_helpers.get_token(id=token.id).owner == from_address
     token_helpers.commit()
-    transferred_token = TokenHelpers().get_token(id=token.id)
+    transferred_token = token_helpers.get_token(id=token.id)
     assert transferred_token.owner == to_address
     _test_token_content_equals(transferred_token, token)
 
 
 def _test_unauthorized_transfer_token(contract_id, certificate_id, from_address, to_address):
     token_helpers = TokenHelpers()
-    token = TokenHelpers().get_token(contract_id=contract_id, certificate_id=certificate_id)
+    token = token_helpers.get_token(contract_id=contract_id, certificate_id=certificate_id)
     token_helpers.transfer_token(contract_id, certificate_id, from_address, to_address)
     token_helpers.commit()
-    transferred_token = TokenHelpers().get_token(id=token.id)
+    transferred_token = token_helpers.get_token(id=token.id)
     assert transferred_token.owner == token.owner
     _test_token_content_equals(token, transferred_token)
 
@@ -181,11 +191,10 @@ def test_transfer_token():
 
 def _test_authorized_claim_token(contract_id, certificate_id, claimer_address):
     token_helpers = TokenHelpers()
-    token = TokenHelpers().get_token(contract_id=contract_id, certificate_id=certificate_id)
+    token = token_helpers.get_token(contract_id=contract_id, certificate_id=certificate_id)
     token_helpers.claim_token(contract_id, certificate_id, claimer_address)
-    assert not TokenHelpers().get_token(id=token.id).is_claimed
     token_helpers.commit()
-    claimed_token = TokenHelpers().get_token(id=token.id)
+    claimed_token = token_helpers.get_token(id=token.id)
     assert claimed_token.is_claimed
     assert claimed_token.claimer == claimer_address
     _test_token_content_equals(token, claimed_token)
@@ -193,13 +202,14 @@ def _test_authorized_claim_token(contract_id, certificate_id, claimer_address):
 
 def _test_unauthorized_claim_token(contract_id, certificate_id, claimer_address):
     token_helpers = TokenHelpers()
-    token = TokenHelpers().get_token(contract_id=contract_id, certificate_id=certificate_id)
+    token = token_helpers.get_token(contract_id=contract_id, certificate_id=certificate_id)
     token_helpers.claim_token(contract_id, certificate_id, claimer_address)
     token_helpers.commit()
-    claimed_token = TokenHelpers().get_token(id=token.id)
+    claimed_token = token_helpers.get_token(id=token.id)
     assert claimed_token.owner == token.owner
     assert not claimed_token.is_claimed
     _test_token_content_equals(token, claimed_token)
+
 
 def test_claim_token():
     contract_id = 1
@@ -215,61 +225,10 @@ def test_claim_token():
     _test_authorized_claim_token(contract_id, certificate_id, valid_claimer_address)
 
 
-def make_log(event_id, event_name, extra_args):
-    args = {
-        'certifID': "0x6d01278accde8008e5c8abdc07fad5",
-    }
-    args.update(extra_args)
-    log = {
-        'args': args,
-        'event': event_name,
-        'logIndex': 0,
-        'transactionIndex': 0,
-        'transactionHash': '0x6d0127880085c8abdc07f5fb21b53ed8b2f5eebe53477b647fd11e6943e8a344',
-        'address': CONTRACT_ADDRESS,
-        'blockHash': '0xde89a5107e263ebb1158ca8366207ebded167848588ca5c3982f2cef85eebcf3',
-        'blockNumber': 1802481,
-        'block': {
-            'timestamp': 1424182926,
-        }
-    }
-    return {
-        'event_id': event_id,
-        'log': log,
-    }
-
-
-def make_new_certificate_log(event_id, event_name=NEW_CERTIFICATE_EVENT_NAME):
-    extra_args = {
-        'owner': '0x13377b14b615fff59c8e66288c32365d38181cdb',
-        'metaData': '0x6d0127880085c8abdc07f5dadb23329bcad23',
-    }
-    return make_log(event_id, event_name, extra_args)
-
-
-def make_transfer_certificate_log(event_id, event_name=TRANSFER_CERTIFICATE_EVENT_NAME):
-    extra_args = {
-        'from': '0x13377b14b615fff59c8e66288c32365d38181cdb',
-        'to': '0x13377b14b615fff59c8e66288c32365d38181cdb',
-    }
-    return make_log(event_id, event_name, extra_args)
-
-
-def make_claim_certificate_log(event_id, event_name=CLAIM_CERTIFICATE_EVENT_NAME):
-    extra_args = {
-        'from': '0x13377b14b615fff59c8e66288c32365d38181cdb',
-    }
-    return make_log(event_id, event_name, extra_args)
-
-
-def make_admin_clean_certificate_log(event_id, event_name=ADMIN_CLEANING_EVENT_NAME):
-    extra_args = {}
-    return make_log(event_id, event_name, extra_args)
-
-
 def _test_invalid_log_insertion(log):
     token_helpers = TokenHelpers()
-    assert token_helpers.insert_log(log) == None
+    assert token_helpers.insert_log(log) is None
+    token_helpers.commit()
 
 
 def _test_valid_log_insertion(log):
--- tests/test_web3.py
+++ tests/test_web3.py
@@ -1,27 +1,44 @@
-from app.web3.helpers import Web3Helpers
+import json
 from app.tokens.helpers import TokenHelpers
-from .testconf import CONTRACT_ADDRESS, CONTRACT_ABI
+from app.web3.helpers import Web3Helpers
+from .testconf import BLOCK_NUMBER, BLOCK, \
+    CONTRACT_ADDRESS, CONTRACT_ABI
 
-"""
-def initialize_db():
-    print("initialize_db")
+def setup_module(module):
+    """Setup database for testing"""
     token_helpers = TokenHelpers()
     token_helpers.init_db()
-    print('db initialized')
     token_helpers.commit()
-    print('db commit')
-    token_helpers.insert_contract(CONTRACT_ADDRESS, CONTRACT_ABI)
-    token_helpers.commit()
-    print("database initialized")
 
 
-def test_update_logs():
-    initialize_db()
-    contract_id = 1
-    web3_helpers = Web3Helpers()
-    web3_helpers.update_logs(contract_id, 10)
+def teardown_module(module):
+    """Tear down database"""
     token_helpers = TokenHelpers()
-    contract = token_helpers.get_contract(contract_id)
-    assert contract.tokens.count() == 0
-    assert contract.last_block == 10
-"""
+    token_helpers.drop_db()
+    token_helpers.commit()
+    token_helpers.close()
+
+
+def test_get_current_block():
+    web3_helpers = Web3Helpers()
+    current_block = web3_helpers.get_current_block_number()
+    assert current_block > 0
+
+
+def test_get_block():
+    web3_helpers = Web3Helpers()
+    block = web3_helpers.get_block(BLOCK_NUMBER)
+    print(block)
+    assert block == BLOCK
+
+
+def test_get_logs():
+    web3_helpers = Web3Helpers()
+    web3_contract = web3_helpers.make_web3_contract(CONTRACT_ADDRESS, json.loads(CONTRACT_ABI.decode()))
+    assert web3_contract is not None
+
+
+
+if __name__ == "__main__":
+    block = Web3Helpers().get_block(1264783)
+    print(block)
