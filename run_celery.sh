bc8568fecbef22c5df10b9c2f22bf3381a3f56b1
Physes <philipsaunders@protonmail.com>
2017-06-21 14:19:18 +0200

    updated solidity contract, abi and configs

--- run_celery.sh
+++ run_celery.sh
@@ -1,6 +1,7 @@
 sleep 6
 
 vertogas init_db
-vertogas insert_contract --address 0x57a0526fbce4183d146be2ef31e16969dacf51bf --abi solidity/0x57a0526fbce4183d146be2ef31e16969dacf51bf/abi.json
+
+vertogas insert_contract --address 0xfC12342Dcf4dc690e03325Cc549c1272EA342092 --abi solidity/0xfC12342Dcf4dc690e03325Cc549c1272EA342092/VertogasRegistrar.json
 vertogas insert_data -p data/biomass.pickle -p data/power_plants.pickle -p data/mixes.pickle
 celery worker -A app --loglevel=INFO -n vertogas1@%h --beat
--- solidity/0xfC12342Dcf4dc690e03325Cc549c1272EA342092/VertogasRegistrar.sol
+++ solidity/0xfC12342Dcf4dc690e03325Cc549c1272EA342092/VertogasRegistrar.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.4.10;
 //0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef
 //1495117646
+
+// at 0xfC12342Dcf4dc690e03325Cc549c1272EA342092
 contract VertogasRegistrar {
-    /* State variables */
-    address public admin;
+
     mapping(bytes32 => certificate) public certifRegistrar;
+    
     uint40 public emittedCertificateCount;
+    address public admin;
 
     struct certificate{
         address owner;
@@ -19,16 +22,34 @@ contract VertogasRegistrar {
         _;
     }
 
+    modifier isValid(bytes32 _certifID) {
+        require(certifRegistrar[_certifID].owner != 0x0);
+        _;
+    }
+
     modifier isAdmin() {
         require(msg.sender == admin);
         _;
     }
 
     /* Events */
-    event NewCertificate(bytes32 metaData, bytes32 certifID, address owner);
-    event Transfer(bytes32 certifID, address indexed from, address indexed to);
-    event Claim(bytes32 certifID, address indexed from);
-    event AdminCleaning(bytes32 certifID);
+    event NewCertificate(
+            bytes32 metaData, 
+            bytes32 certifID, 
+            address owner);
+
+    event Transfer(
+            bytes32 certifID, 
+            address indexed from, 
+            address indexed to);
+
+    event Claim(
+            bytes32 certifID, 
+            address indexed from);
+
+    event AdminCleaning(
+            bytes32 certifID);
+
 
     /* Constructor and destructor and ...*/
     function VertogasRegistrar() {
@@ -44,14 +65,16 @@ contract VertogasRegistrar {
     }
 
     /* Methods */
-    function newCertificate(bytes32 _metaData, address _owner) isAdmin {
+    function newCertificate(bytes32 _metaData, address _owner) {
              emittedCertificateCount++;
              bytes32 _certifID = sha3(_metaData, emittedCertificateCount); //certifID are therefore unique
              certifRegistrar[_certifID] = certificate(_owner, now + 1 years);
              NewCertificate(_metaData, _certifID, _owner);
     }
 
-    function transfer(bytes32 _certifID, address _to) isAllowed(_certifID) {
+    function transfer(bytes32 _certifID, address _to) 
+                            isAllowed(_certifID)
+                            isValid(_certifID) {
              certifRegistrar[_certifID].owner = _to;
              Transfer(_certifID, msg.sender, _to);
     }
@@ -64,12 +87,17 @@ contract VertogasRegistrar {
              return certifRegistrar[_certifID].timeLimit;
     }
 
-    function claim(bytes32 _certifID) isAllowed(_certifID) {
+    function claim(bytes32 _certifID) 
+                            isAllowed(_certifID)
+                            isValid(_certifID) {
              delete(certifRegistrar[_certifID]);
+             emittedCertificateCount--;
              Claim(_certifID, msg.sender);
     }
 
-    function adminCleaning(bytes32 _certifID) isAdmin {
+    function adminCleaning(bytes32 _certifID) 
+                                    isAdmin 
+                                    isValid(_certifID) {
              delete(certifRegistrar[_certifID]);
              AdminCleaning(_certifID);
     }
